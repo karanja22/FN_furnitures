72680d19a9e723cf19ca030878f650c17ca260b3
karanja22 <frasiahnyambura22@gmail.com>
2025-05-05 16:27:57 +0300

    recreating nestjs

--- Nestjs/src/app.module.ts
+++ Nestjs/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [ProductsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL, 
+      ssl: { rejectUnauthorized: false },
+      entities: [ProductsModule],
+      synchronize: true, 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
